e9bec7ef1c5ffa6b7f98825b7a2ea671c4d29b66
kevinouahmad <40160995+kevinouahmad@users.noreply.github.com>
2018-06-11 17:38:44 +0200

    Add files via upload

---
+++ Monument Go/model/User.php
@@ -0,0 +1,106 @@
+<?php 
+
+class User
+{
+	private $_id;
+	private $_lastName;
+	private $_firstName;
+	private $_mail;
+	private $_phone;
+	private $_postalCode;
+	private $_street;
+	private $_streetNumber;
+	private $_town;
+	private $_country;
+
+	public function id()
+	{
+		return $this->_id;
+	}
+	public function lastName()
+	{
+		return $this->_lastName;
+	}
+	public function firstName()
+	{
+		return $this->_firstName;
+	}
+	public function mail()
+	{
+		return $this->_mail;
+	}
+	public function phone()
+	{
+		return $this->_phone;
+	}
+	public function postalCode()
+	{
+		return $this->_postalCode;
+	}
+	public function street()
+	{
+		return $this->_street;
+	}
+	public function streetNumber()
+	{
+		return $this->_streetNumber;
+	}
+	public function town()
+	{
+		return $this->_town;
+	}
+	public function country()
+	{
+		return $this->_country;
+	}
+
+	public function setLastName($lastName)
+	{
+		$this->_lastName = $lastName;
+	}
+	public function setFirstName($firstName)
+	{
+		$this->_firstName = $firstName;
+	}
+	public function setMail($mail)
+	{
+		$this->_mail = $mail;
+	}
+	public function setPhone($phone)
+	{
+		$this->_phone = $phone;
+	}
+	public function setPostalCode($postalCode)
+	{
+		$this->_postalCode = $postalCode;
+	}
+	public function setStreet($street)
+	{
+		$this->_street = $street;
+	}
+	public function setStreetNumber($streetNumber)
+	{
+		$this->_streetNumber = $streetNumber;
+	}
+	public function setTown($town)
+	{
+		$this->_town = $town;
+	}
+	public function setCountry($country)
+	{
+		$this->_country = $country;
+	}
+
+	public function hydrate(array $donnees)
+	{
+	  	foreach ($donnees as $key => $value)
+	  	{
+	    	$method = 'set'.ucfirst($key);
+	        
+	    	if (method_exists($this, $method))
+	    	{
+	      		$this->$method($value);
+	    	}
+	  	}
+	}
+}
---
+++ Monument Go/model/UsersManager.php
@@ -0,0 +1,99 @@
+<?php
+
+require('User.php');
+
+class UsersManager
+{
+
+	private $_db;
+
+	public function __construct($db)
+	{
+		$this->setDb($db);
+	}
+
+	public function setDb(PDO $db)
+  	{
+    	$this->_db = $db;
+  	}
+
+
+	public function get($idUser)
+	{
+        $id = (int) $id;
+
+    	$q = $this->_db->query('SELECT * FROM users WHERE id = '.$id);
+    	$donnees = $q->fetch(PDO::FETCH_ASSOC);
+
+    	return new User($donnees);
+	}
+
+	public function add(User $usr)
+	{
+		$q = $this->_db->prepare('INSERT INTO users(lastName, firstName, mail, phone, postalCode, street, streetNumber, town, country) VALUES(:lastName, :firstName, :mail, :phone, :postalCode, :street, :streetNumber, :town, :country)');
+
+	    $q->bindValue(':lastName', $usr->lastName());
+	    $q->bindValue(':firstName', $usr->firstName());
+	    $q->bindValue(':mail', $usr->mail());
+	    $q->bindValue(':phone', $usr->phone());
+	    $q->bindValue(':postalCode', $usr->postalCode());
+	    $q->bindValue(':street', $usr->street());
+	    $q->bindValue(':streetNumber', $usr->streetNumber());
+	    $q->bindValue(':town', $usr->town());
+	    $q->bindValue(':country', $usr->country());
+
+	    $q->execute();
+	}
+
+	public function delete(User $usr)
+	{
+		$this->_db->exec('DELETE FROM users WHERE id = '.$usr->id());
+	}
+
+	public function update(User $usr)
+  	{
+    	$q = $this->_db->prepare('UPDATE users SET lastName = :lastName, firstName = :firstName, mail = :mail, phone = :phone, postalCode = :postalCode, street = :street, streetNumber = :streetNumber, town = :town, country = :country WHERE id = :id');
+
+    	$q->bindValue(':lastName', $usr->lastName());
+    	$q->bindValue(':firstName', $usr->firstName());
+    	$q->bindValue(':mail', $usr->mail());
+    	$q->bindValue(':phone', $usr->phone());
+    	$q->bindValue(':postalCode', $usr->postalCode());
+    	$q->bindValue(':street', $usr->street());
+    	$q->bindValue(':streetNumber', $usr->streetNumber());
+    	$q->bindValue(':town', $usr->town());
+    	$q->bindValue(':country', $usr->country());
+
+    	$q->execute();
+  	}
+
+	public function getList()
+	{
+        $users = [];
+		$q = $this->_db->query('SELECT * FROM users');	
+
+		while ($donnees = $q->fetch()) {
+			$users[] = new User($donnees);
+		}
+		return $users;
+	}
+}
+
+/*$usrManager = new UsersManager(new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', ''));
+
+$users = $usrManager->getList();
+
+foreach ($users as $user) {
+	?>
+
+	<?= $user->lastName(); ?>
+
+	<?php	
+}*/
+
+$PDO = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+
+$q = $PDO->query('SELECT * FROM users');
+while ($data = $q->fetch()) {
+	echo $data['id'];
+}
